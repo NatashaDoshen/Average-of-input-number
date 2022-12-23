4f69627f7ef6057a4925fde91c9a828c8729f914
Natasha Doshen <46009117+NatashaDoshen@users.noreply.github.com>
2022-12-23 14:58:25 +0000

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,9 @@ print(excluding_number)
 
 avg = excluding_number/len(list_of_numbers)
 print(avg)
+
+
+
+Write a program that always asks the user to enter a number. When the user enters the negative number -1, the program should stop requesting the user to enter a number. The program must then calculate the average of the numbers entered excluding the -1.
+
+Make use of the while loop repetition structure to implement the program.
